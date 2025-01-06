92b94cf8634f0fb2194e171300ead9875cf6918f
Luis Galeana <lsgaleana@gmail.com>
2025-01-06 08:10:14 -0600

    Design prompt update

--- workflows/design.py
+++ workflows/design.py
@@ -39,27 +39,6 @@ class ComponentToUpdate(BaseModel):
     base: Union[Component, str]
 
 
-COMMANDS_LINE = """To add or update a component, use the following format:
-
-```json
-{{
-    "action": "add", "update" or "remove",
-    "type": "dbmodel" or "function",
-    # Attributes of the dbmodel or function
-}}
-```
-
-To remove a component, use the following format:
-
-```json
-{{
-    "action": "remove",
-    "name": "The name of the function to remove"
-    "namespace": "The namespace of the function to remove"
-}}
-```"""
-
-
 def run(app_name: str, user_message: str) -> Tuple[Dict[str, Any], Conversation]:
     config = load_config(app_name)
     conversation = Conversation.load(app_name)
@@ -106,9 +85,27 @@ The architecture that you're working with is a python module that will be hosted
 ]
 ```
 
-Think of this architecture as lego blocks that you can compose together. Follow the user's instructions to build the architecture by adding, updating or removing components.
+Think of this architecture as lego blocks that you can compose together. Use a modular design pattern. Too many steps in a function's purpose probably means that you should break it apart. Always prefer the most simple design.
 
-Use a modular design pattern. Too many steps in a function's purpose probably means that you should break it apart. Always prefer the most simple design.
+Follow the user's instructions to build the architecture by adding, updating or removing components. To add or update a component, use the following format:
+
+```json
+{{
+    "action": "add", "update" or "remove",
+    "type": "dbmodel" or "function",
+    # Attributes of the dbmodel or function
+}}
+```
+
+To remove a component, use the following format:
+
+```json
+{{
+    "action": "remove",
+    "name": "The name of the function to remove"
+    "namespace": "The namespace of the function to remove"
+}}
+```
 
 There are two types of "design" components: dbmodels and functions. functions can be added, updated or removed at any time. However, dbmodels can only be added, updated or removed if they haven't been deployed yet. Updating production database models is not straightforward. To update or remove a dbmodel, the user must do it manually.
 
@@ -118,8 +115,7 @@ IMPORTANT: The modassembly namespace is reserved. You can't add or update compon
         )
 
     conversation.add_system(
-        f"{COMMANDS_LINE}\n\nCurrent architecture:\n\n"
-        f"{present_to_llm(list(architecture.values()))}\n\n"
+        f"Current architecture:\n\n{present_to_llm(list(architecture.values()))}\n\n"
         "Think whether the architecture that you're describing is the best for what the user wants to do."
     )
     conversation.add_user(user_message)
