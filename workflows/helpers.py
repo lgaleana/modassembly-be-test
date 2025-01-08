0f8c59414c80b9b584a1dae52c1e9de60675fb00
Luis Galeana <lsgaleana@gmail.com>
2025-01-08 08:52:08 -0600

    Update submodules

--- workflows/helpers.py
+++ workflows/helpers.py
@@ -151,7 +151,6 @@ def install_requirements(
         check=False,
         capture_output=True,
         text=True,
-        cwd=f"{REPOS}/{app_name}"
     )
     print_system(output.stdout)
     print_system(output.stderr)
