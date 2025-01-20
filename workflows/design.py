b1cee82a58876249273773e4abed670227102587
Luis Galeana <lsgaleana@gmail.com>
2025-01-19 22:02:48 -0600

    Update design prompt

--- workflows/design.py
+++ workflows/design.py
@@ -114,7 +114,7 @@ There are three types of "design" components: infrastructures, datamodels and fu
 
 Think of datamodels as data sinks. They represent database tables. To add datamodels you must first have the external infrastructure to support it.
 
-At some point, every component will be implemented into actual code (you don't have access to that code). All of it will be executed on Google Cloud Run, except for the infrastructures. Cloud Run is a servelerss container desgined for web applications. More complex infrastructure has to be run seperately. "infrastructure" represents all the external infrastructure that is part of your backend architecture but that won't be run on Cloud Run. Nonetheless, Cloud Run has access to it. It's analogous to the GCP infrastructure.
+At some point, every component will be implemented into actual code (you don't have access to that code). All of it will be executed on Google Cloud Run, except for the infrastructures. More complex infrastructure has to be run seperately. "infrastructure" represents all the external infrastructure that is part of your backend architecture but that won't be run on Cloud Run. It's analogous to the GCP infrastructure.
 
 What this means is that you must be careful about how you design your business logic. Keep it within the limitations of a web service. For anything else, rely on the available external infrastructure. As you add infrastructure, utility functions will be added so that your application can connect to it. You can use APIs directly.
 
@@ -122,7 +122,7 @@ functions represent the business logic. The main goal is to design an architectu
 
 Make sure that the composition of all function purposes describe the entire architecture.
 
-The `"dependencies"` attribute is VERY IMPORTANT. As you add components, add them in the order of their dependencies. When each component is implemented into code, the order of implementation will be guided by them. Keep them updated."""
+The `"dependencies"` attribute is VERY IMPORTANT. As you add components, add them in the order of their dependencies. When each component is implemented into code, the order of implementation will be guided by the dependencies. Always update them."""
 
 
 def run(
@@ -222,10 +222,10 @@ def run(
                         component = Component.model_validate(json_)
                         if (
                             isinstance(component.root, DataModel)
-                            and "External.CloudSQL" not in architecture
-                            and "External.CloudStorage" not in architecture
-                            and "External.CloudSQL" not in components_to_update
-                            and "External.CloudStorage" not in components_to_update
+                            and "External.CloudSQLDatabase" not in architecture
+                            and "External.CloudStorageBucket" not in architecture
+                            and "External.CloudSQLDatabase" not in components_to_update
+                            and "External.CloudStorageBucket" not in components_to_update
                         ):
                             raise ValueError(
                                 f"Unable to {action} component :: {key} "
